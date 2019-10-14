ca69b44ce4749c1f193b8266d45f265b4b68ec68
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-10-14 16:02:36 +0200

    Creating the open-close dialog functionality

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -3,10 +3,39 @@ import { Link } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { withStyles } from '@material-ui/styles';
+import Dialog from '@material-ui/core/Dialog';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
 import styles from './styles/PaletteListStyles';
 
 
 class PaletteList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openDeleteDialog: false
+    }
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+  }
+
+  openDialog() {
+    this.setState({ openDeleteDialog: true })
+  }
+
+  closeDialog() {
+    this.setState({ openDeleteDialog: false })
+  }
+
+
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`)
   }
@@ -36,8 +65,32 @@ class PaletteList extends Component {
               </CSSTransition>
             ))}
           </TransitionGroup>
-
         </div>
+        <Dialog
+          open={true}
+          aria-labelledby="delete-dialog-title">
+          <DialogTitle id="delete-dialog-title">Delete This Palette?</DialogTitle>
+          <List>
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar
+                  style={{ backgroundColor: blue[100], color: blue[600] }}>
+                  <CheckIcon />
+                </Avatar >
+              </ListItemAvatar>
+              <ListItemText>Delete</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemAvatar>
+                <Avatar
+                  style={{ backgroundColor: red[100], color: red[600] }}>
+                  <CloseIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText>Cancel</ListItemText>
+            </ListItem>
+          </List>
+        </Dialog>
       </div>
     )
   }
